a776d6ab7ca21f580a324de2bbfbb4c2745d440f
A.karak <106385729+karak02@users.noreply.github.com>
2024-09-14 17:09:49 +0000

    browser done

--- jobpotarl/frontend/src/components/pages/Browser.jsx
+++ jobpotarl/frontend/src/components/pages/Browser.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Navbar from '../shared/Navbar'
+import Job from './jobs/Job';
+import Footer from '../shared/Footer';
+
+const randomjobs = [1,2,3,4,5];
 
 const Browser = () => {
   return (
-    <div>
+    <>
         <Navbar/>
-        <h1>Browser</h1>
-    </div>
+      <div className='max-w-7xl mx-auto my-10'>
+        <h1 className='font-bold text-2xl mb-4'>Search Result ({randomjobs.length})</h1>
+        <div className='grid grid-cols-3 gap-4'>
+        {
+          randomjobs.map((item,index)=>{
+            return (
+              <Job/>
+            )
+          })
+        }
+        </div>
+      </div>
+      <Footer/>
+    </>
   )
 }
 
